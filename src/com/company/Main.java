58346e5a344ed401c1ef96dc9563118de740e77a
ha271097 <nguyenquangha2710@gmail.com>
2021-04-04 17:10:17 +0700

    4/4/21

---
+++ src/com/company/Main.java
@@ -0,0 +1,28 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int a;
+        int b;
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter a");
+        a = sc.nextInt();
+        System.out.println("Enter b");
+        b = sc.nextInt();
+//        a = Math.abs(a);
+//        b = Math.abs(b);
+        if (a == 0 || b == 0)
+            System.out.println("No greatest common factor");
+        while (a != b) {
+            if (a > b)
+                a = a - b;
+            else
+                b = b - a;
+        }
+        System.out.println( a);
+    }
+}
